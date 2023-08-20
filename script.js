b36740b3e3aafd88df678df3a341a2ff8ed7f7ea
nathan-j-lee <127238376+nathan-j-lee@users.noreply.github.com>
2023-08-20 16:53:13 -0700

    Add basic operation functions

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 var firstNumber;
 var operation;
 var secondNumber;
+var result;
+
+function operate(operation, firstNumber, secondNumber) {
+    this.operation = operation;
+    this.firstNumber = firstNumber;
+    this.secondNumber = secondNumber;
+}
+
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+
+function subtract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+}
+
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+
+function divide(firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
+}
